0498c93118a5a55ffb74b1ae42f46d9277bdc602
Marwan Elsayed <marwanellsayed@hotmail.com>
2025-01-24 23:05:58 +0200

    feat: add API constants for Unsplash base URL and access key

---
+++ src/lib/constants/api.constants.ts
@@ -0,0 +1,2 @@
+export const BASE_URL = process.env.REACT_APP_BASE_URL || "https://api.unsplash.com"; // should be in .env file
+export const UNSPLASH_ACCESS_KEY = process.env.REACT_APP_UNSPLASH_ACCESS_KEY || "6FfROqtHfJTUW5xwupX5ghiE_M4x1xDOPJyvJ1S_41I"; // should be in .env file
