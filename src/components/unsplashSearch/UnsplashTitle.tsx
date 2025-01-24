471e7373c49822dfa120e2a214f9a29d7af1188a
Marwan Elsayed <marwanellsayed@hotmail.com>
2025-01-24 23:05:35 +0200

    feat: add UnsplashTitle component for displaying the main title of the image search page

---
+++ src/components/unsplashSearch/UnsplashTitle.tsx
@@ -0,0 +1,3 @@
+export default function UnsplashTitle() {
+	return <h1 className='text-4xl font-bold text-center mb-8 text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600'>Unsplash Image Search</h1>;
+}
